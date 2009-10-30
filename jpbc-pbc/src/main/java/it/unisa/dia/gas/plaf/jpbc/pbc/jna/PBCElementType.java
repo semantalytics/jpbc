cefb6f7a761bdee03fd3ae1d0ea614fb69a86eb1
adecaro <adecaro@169d89c2-a4f7-4cc7-9cfa-6d6735fd7f57>
2009-10-30 07:54:51 +0000

    minor updates...

--- jpbc-pbc/src/main/java/it/unisa/dia/gas/plaf/jpbc/pbc/jna/PBCElementType.java
+++ jpbc-pbc/src/main/java/it/unisa/dia/gas/plaf/jpbc/pbc/jna/PBCElementType.java
@@ -36,6 +36,6 @@ public class PBCElementType extends Memory {
     @Override
     protected void finalize() {
         PBCLibraryProvider.getPbcLibrary().pbc_element_clear(this);
-        super.finalize();
+//        super.finalize();
     }
 }
