fc1be32fa044c40f919caa3aa5c24a03c4bfc25a
adecaro <adecaro@169d89c2-a4f7-4cc7-9cfa-6d6735fd7f57>
2010-10-13 13:44:38 +0000

    minor updates...

---
+++ jpbc-crypto/src/main/java/it/unisa/dia/gas/plaf/jpbc/crypto/utils/PairingInfo.java
@@ -0,0 +1,26 @@
+package it.unisa.dia.gas.plaf.jpbc.crypto.utils;
+
+import it.unisa.dia.gas.jpbc.Pairing;
+import it.unisa.dia.gas.plaf.jpbc.pairing.CurveParams;
+import it.unisa.dia.gas.plaf.jpbc.pairing.PairingFactory;
+
+/**
+ * @author Angelo De Caro (angelo.decaro@gmail.com)
+ */
+public class PairingInfo {
+
+    public static void main(String[] args) {
+        CurveParams curveParams = new CurveParams();
+        curveParams.load(args[0]);
+
+        Pairing pairing = PairingFactory.getInstance().initPairing(curveParams);
+        System.out.println("pairing.isSymmetric() = " + pairing.isSymmetric());
+        System.out.println("pairing.getZr().getLengthInBytes() = " + pairing.getZr().getLengthInBytes());
+        System.out.println("pairing.getG1().getLengthInBytes() = " + pairing.getG1().getLengthInBytes());
+        System.out.println("pairing.getG2().getLengthInBytes() = " + pairing.getG2().getLengthInBytes());
+        System.out.println("pairing.getGT().getLengthInBytes() = " + pairing.getGT().getLengthInBytes());
+        System.out.println("pairing.getG1().newElement().getLengthInBytesCompressed() = " + pairing.getG1().newElement().getLengthInBytesCompressed());
+        System.out.println("pairing.getG2().newElement().getLengthInBytesCompressed() = " + pairing.getG2().newElement().getLengthInBytesCompressed());
+    }
+
+}
