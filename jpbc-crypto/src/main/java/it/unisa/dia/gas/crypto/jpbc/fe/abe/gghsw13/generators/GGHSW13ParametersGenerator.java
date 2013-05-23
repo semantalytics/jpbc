35b1e524bcacdb25f766c7ddc28e7c04f59e02b7
adecaro <adecaro@169d89c2-a4f7-4cc7-9cfa-6d6735fd7f57>
2013-05-23 02:06:02 +0000

    minor updates

--- jpbc-crypto/src/main/java/it/unisa/dia/gas/crypto/jpbc/fe/abe/gghsw13/generators/GGHSW13ParametersGenerator.java
+++ jpbc-crypto/src/main/java/it/unisa/dia/gas/crypto/jpbc/fe/abe/gghsw13/generators/GGHSW13ParametersGenerator.java
@@ -1,6 +1,7 @@
 package it.unisa.dia.gas.crypto.jpbc.fe.abe.gghsw13.generators;
 
 import it.unisa.dia.gas.crypto.jpbc.fe.abe.gghsw13.params.GGHSW13Parameters;
+import it.unisa.dia.gas.plaf.jpbc.mm.clt13.engine.CTL13MMInstance;
 import it.unisa.dia.gas.plaf.jpbc.mm.clt13.generators.CTL13MMInstanceGenerator;
 import it.unisa.dia.gas.plaf.jpbc.mm.clt13.pairing.CTL13MMPairing;
 import it.unisa.dia.gas.plaf.jpbc.mm.clt13.parameters.CTL13MMInstanceParameters;
@@ -14,24 +15,42 @@ import java.security.SecureRandom;
 public class GGHSW13ParametersGenerator {
 
     private SecureRandom random;
+    private CTL13MMInstance instance;
     private CTL13MMInstanceParameters parameters;
     private int n;
 
     public GGHSW13ParametersGenerator init(SecureRandom random, CTL13MMInstanceParameters parameters, int n) {
         this.random = random;
         this.parameters = parameters;
+        this.instance = null;
+        this.n = n;
+
+        return this;
+    }
+
+    public GGHSW13ParametersGenerator init(SecureRandom random, CTL13MMInstance instance, int n) {
+        this.random = random;
+        this.parameters = null;
+        this.instance = instance;
         this.n = n;
 
         return this;
     }
 
     public GGHSW13Parameters generateParameters() {
-        return new GGHSW13Parameters(
-                new CTL13MMPairing(
-                        random,
-                        new CTL13MMInstanceGenerator(random, parameters).generateInstance()
-                ),
-                n);
+        if (parameters != null)
+            return new GGHSW13Parameters(
+                    new CTL13MMPairing(
+                            random,
+                            new CTL13MMInstanceGenerator(random, parameters).generateInstance()
+                    ),
+                    n);
+        else if (instance != null) {
+            return new GGHSW13Parameters(
+                    new CTL13MMPairing(random, instance), n
+            );
+        } else
+            throw new IllegalStateException("Generator not initialized!");
     }
 
 }
