25c5574e1381028d30f0b0ebcf94dc7a55adf31e
adecaro <adecaro@169d89c2-a4f7-4cc7-9cfa-6d6735fd7f57>
2014-03-23 08:58:35 +0000

    updates

--- jpbc-lattice/src/test/java/it/unisa/dia/gas/crypto/jlbc/trapdoor/mp12/engines/MP12HLP2Test.java
+++ jpbc-lattice/src/test/java/it/unisa/dia/gas/crypto/jlbc/trapdoor/mp12/engines/MP12HLP2Test.java
@@ -34,7 +34,7 @@ public class MP12HLP2Test {
         System.out.println("MP12HLP2Test.setUp.START");
 
         gen = new MP12HLP2KeyPairGenerator();
-        gen.init(new MP12HLP2KeyPairGenerationParameters(random, 4, 2));
+        gen.init(new MP12HLP2KeyPairGenerationParameters(random, 4));
         keyPair = gen.generateKeyPair();
 
         System.out.println("MP12HLP2Test.setUp.END");
